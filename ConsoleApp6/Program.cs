597a37cddd36bcc04c58105d7497ee7be42419c3
Patrick Wong <pwong@nimbyx.com>
2024-08-11 23:18:48 +0800

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,13 @@
+﻿using BCrypt.Net;
+
+
+class Program
+{
+    static void Main()
+    {
+        string password = "AdminPassword123"; // Replace with your desired password
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+        System.Console.WriteLine("Hashed Password: " + hashedPassword);
+    }
+}
